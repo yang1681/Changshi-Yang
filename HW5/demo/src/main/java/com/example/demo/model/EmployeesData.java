2b13dd1d60a37cfcb4f0093b8f534c1081ca78b7
yang1681 <44850278+yang1681@users.noreply.github.com>
2022-08-28 11:56:09 -0500

    Update EmployeesData.java

--- HW5/demo/src/main/java/com/example/demo/model/EmployeesData.java
+++ HW5/demo/src/main/java/com/example/demo/model/EmployeesData.java
@@ -4,14 +4,7 @@ import lombok.Data;
 
 import java.util.List;
 
-/**
- * Title: <br>
- * Description: <br>
- * Company: wondersgroup.com <br>
- *
- * @author 袁浩霆
- * @version 1.0
- */
+
 @Data
 public class EmployeesData {
 
