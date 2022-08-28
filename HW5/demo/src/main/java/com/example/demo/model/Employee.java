42a173d270ba873b304ce046653d47bf216a140f
yang1681 <44850278+yang1681@users.noreply.github.com>
2022-08-28 11:56:28 -0500

    Update Employee.java

--- HW5/demo/src/main/java/com/example/demo/model/Employee.java
+++ HW5/demo/src/main/java/com/example/demo/model/Employee.java
@@ -5,14 +5,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
-/**
- * Title: <br>
- * Description: <br>
- * Company: wondersgroup.com <br>
- *
- * @author 袁浩霆
- * @version 1.0
- */
+
 @Data
 public class Employee {
 
