66316d4c3cc2185f0e5359af655bce7bf56a2c93
yang1681 <44850278+yang1681@users.noreply.github.com>
2022-09-08 15:20:25 -0500

    Add files via upload

---
+++ HW/fizzbuzz/solution.java
@@ -0,0 +1,35 @@
+package com.example.java17il2022.week6.fizzbuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class solution {
+
+    
+    public List<String> fizzbuzzHelper(int n){
+        List<String> ans = new ArrayList<String>();
+
+        for (int num = 1; num <= n; num++) {
+
+            boolean divisibleBy3 = (num % 3 == 0);
+            boolean divisibleBy5 = (num % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                // Divides by both 3 and 5, add FizzBuzz
+                ans.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                // Divides by 3, add Fizz
+                ans.add("Fizz");
+            } else if (divisibleBy5) {
+                // Divides by 5, add Buzz
+                ans.add("Buzz");
+            } else {
+                // Not divisible by 3 or 5, add the number
+                ans.add(Integer.toString(num));
+            }
+        }
+
+        return ans;
+    }
+   
+}
